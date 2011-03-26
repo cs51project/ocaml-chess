dac3dd23a333eae8b3d8477cd01faf06508a47b3
Amy_Yin <amyin@college.harvard.edu>
2011-03-26 14:35:46 -0400

    added "capture" and removed one of the two moves defined in board.ml

--- board.ml
+++ board.ml
@@ -7,13 +7,14 @@ sig
   type move = Move of ( piece_type * piece_position ) | Castle of castle  
   type board
 
-  val init_board : board (* standard starting board *)
+  val init_board : unit -> board (* standard starting board *)
 
   val all_moves : board -> move list 
     (* all valid moves *)
   val all_pieces : board -> piece list 
     (* all pieces on current board *)
-  val move : board -> move -> board option 
+  val capture : play -> piece option (* returns piece captured or None *)
+  val play : board -> move -> board option 
     (* should return None if the move is invalid *)
   val check : dict -> color option 
     (* returns color in position to win *)
@@ -23,10 +24,13 @@ end
 
 module ChessSet(B: BOARD) : (SET with type piece = B.t) = 
   struct
-    type piece_position = int * int
+    open Pieces;;
+    type piece_position = int * int (* 0,0--lower lt; 7,7--upper rt *)
     type piece_type = Pawn | Knight | Bishop | Rook | Queen | King
     type piece_color = Black | White
     type piece = {name : piece_type; color: piece_color; position: piece_position}
-    type move = Move of ( piece_type * piece_position ) | Castle of castle 
+    type move = Move of ( piece_type * piece_position ) | Castle of castle    type board = piece list (* list of active pieces *)
+    let init_board () =  [wr1;...;br2];
+    let all_moves (board: board) : move list = [Castle]; (*incomplete*)
     
    end
