7367013f9336d24369fd85fa1bf1f13f40be211f
David Palmer <dpalmer@college.harvard.edu>
2011-03-26 06:14:24 -0400

    defined basic board interface

--- board.ml
+++ board.ml
@@ -1,19 +1,17 @@
 module type BOARD = 
 sig
-  type key (* position *)   
-  type value (* piece *)
-  type color 
-  type dict 
-  val empty : dict 
-  val start : dict
-  val move : dict -> key -> value -> color -> dict option
-    (* should return none if the move is invalid *)
-    (* if the move is valid, take the dict and check if the move captures another piece *)
-  val capture: dict -> key -> value -> color -> dict
-    (* must check if piece is opposite color; else returns original board *)
-  val lookup : dict -> key -> color -> (value * color) option
-  val remove : dict -> key -> dict
-  val check : dict -> color option (* returns color that is in position to win *)
-  val checkmate: dict -> color option (* checks if the game is over *)
-  val fold : (key -> value -> 'a -> 'a) -> 'a -> dict -> 'a
+  type piece_position = int * int
+  type piece_type = Pawn | Knight | Bishop | Rook | Queen | King
+  type piece_color = Black | White
+  type piece = {name : piece_type; color: piece_color; position: piece_position}
+  type move = Standard of piece * piece_position | Castle of castle | 
+  type board
+
+  val std_board : board (* standard starting board *)
+
+  val all_moves : board -> move list (* all valid moves *)
+  val all_pieces : board -> piece list (* all pieces on current board *)
+  val move : board -> move -> board option (* should return None if the move is invalid *)
+  val check : dict -> color option (* returns color in position to win *)
+  val checkmate: dict -> color option (* checks whether the game is over *)
 end
