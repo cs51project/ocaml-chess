0b14b14dd1a9c060f412e40c3f2cdf4aab7e4b39
David Palmer <dpalmer@college.harvard.edu>
2011-03-29 18:44:10 -0400

    enforced position abstraction and converted implementation

--- board.ml
+++ board.ml
@@ -1,6 +1,5 @@
 module type BOARD = 
 sig
-  (* a1 is (0, 0); h8 is (7, 7) *)
   type position
   type piece_type = Pawn | Knight | Bishop | Rook | Queen | King
   type piece = Black of piece_type | White of piece_type
@@ -11,8 +10,14 @@ sig
   type board
     (* add the not valid position exception *)
 
-  (* build position *)
-  val create_pos : (int * int) -> Pos
+  (* build position from pair of integers
+   * (i.e. create_pos rank file,
+   * where rank, file are between 0 and 7
+   * raise exception if invalid coordinates *)
+  val create_pos : int -> int -> position
+  (* functions for manipulating positions *)
+  val neighbor : int -> int -> position -> position option
+  val vector : position -> position -> (int * int)
   (* standard starting board *)
   val init_board : board
   (* which color is to play *)
@@ -46,34 +51,50 @@ struct
 
   module PositionMap = Map.Make(struct
       type t = position
-      let compare p1 p2 =
-        let (p1, p2) = ((p1r, p1f), (p2r, p2f)) in
-          if p1r < p2r then -1
-          else if p1r > p2r then 1
-          else if p1r = p2r then
-            if p1f < p2f then -1
-            else if p1f > p2f then 1
+      let compare pos1 pos2 =
+        let (Pos(r1, f1), Pos(r2, f2)) = (pos1, pos2)  in
+          if r1 < r2 then -1
+          else if r1 > r2 then 1
+          else if r1 = r2 then
+            if f2 < f2 then -1
+            else if f1 > f2 then 1
             else 0
     end)
 
-  (* a board is a map of location => piece together with extra data *)
+  (* a board is a map of positions to pieces together with extra data *)
   type board_config = {toPlay : side;}
   type board = (piece PositionMap.t) * board_config 
   
-  let create_pos (x:int) (y:int) : position =
-    if (x>=0&&x<=7) && (y>=0&&y<=7) then Pos (x,y) 
+  let create_pos rank file : position =
+    if (rank >= 0 && rank <= 7) && (file >= 0 && file <= 7) then
+      Pos (x, y)
     else raise Exception "Not a valid position"
+
+  let neighbor dx dy pos =
+    let Pos(r0, f0) = pos in
+    let (r1, f1) = (r0 + dr, f0 + df) in
+    if (r1 >= 0 && r1 <= 7) && (f1 >= 0 && f1 <= 7) then
+      Some Pos (r1, f1)
+    else
+      None
+
+  let vector pos1 pos2 =
+    let (Pos(rank1, file1), Pos(rank2, file2)) = (pos1, pos2) in
+      (rank2 - rank1, file2 - file1)
+
   let init_board = 
     let files = [0; 1; 2; 3; 4; 5; 6; 7] in
     let names = [Rook; Knight; Bishop; Queen; King; Bishop; Knight; Rook] in
     let pc_files = List.combine files names in
     let init_pcs = List.fold_left
-      (fun r (x, pc) -> ((7, x), Black pc) :: ((0, x), White pc) :: r) [] pc_files in
+      (fun r (x, pc) -> (create_pos 7 x, Black pc) :: 
+      (create_pos 0 x, White pc) :: r) [] pc_files in
     let init_pawns = List.fold_left 
-      (fun r x -> ((6, x), Black Pawn) :: ((1, x), White Pawn) :: r) [] files in
+      (fun r x -> (create_pos 6 x, Black Pawn) ::
+      (create_pos 1 x, White Pawn) :: r) [] files in
     let init_bindings = init_pcs @ init_pawns in
-    let add_binding board ((x, y), pc) =
-      PositionMap.add (x, y) pc board
+    let add_binding board (pos, pc) =
+      PositionMap.add pos pc board
     in
       (List.fold_left add_binding PositionMap.empty init_bindings,
         {toPlay = White King})
@@ -92,36 +113,30 @@ struct
     let (map, cfg) = b in
       PartitionMap.bindings map
 
-  let piece2fn p = 
-    match p with 
+  (* helper functions for is_valid *)
+  let is_valid_for pc = 
+    match pc with 
       | Pawn -> is_valid_pawn
       | Knight -> is_valid_knight
       | Bishop -> is_valid_bishop
-      | ...
+      | Rook -> is_valid_rook
+      | Queen -> is_valid_queen
+      | King -> is_valid_queen
+
+  let is_valid_pawn b mv dir =
+    let (pos1, pos2) = mv in
+    let (dr, df) = vector pos1 pos2 in
+    if dr != dir then false
+    else 
+    
 
   let is_valid b mv =
     match mv with
-      | Standard (p1, p2) ->
-	  let pc = piece_at b p1 in
+      | Standard (pos1, pos2) ->
+          let pc = piece_at b pos1 in
 	    match pc with
-	      | White p -> (piece2fn p) b mv 1
-	      | Black p -> (piece2fn p) b mv -1
-	  
-          let pc = piece_at b p1 in
-            (match pc with
-               | White Pawn -> is_valid_pawn b mv 1
-               | Black Pawn -> is_valid_pawn b mv -1
-               | White Knight -> is_valid_knight b mv 1
-               | Black Knight -> is_valid_knight b mv -1
-               | White Bishop -> is_valid_bishop b mv 1
-               | Black Bishop -> is_valid_bishop b mv -1
-               | White Rook -> is_valid_rook b mv 1
-               | Black Rook -> is_valid_rook b mv -1
-               | White Queen -> is_valid_queen b mv 1
-               | Black Queen -> is_valid_queen b mv -1
-               | White King -> is_valid_king b mv 1
-               | Black King -> is_valid_king b mv -1
-            )
+	      | White pc -> is_valid_for pc b mv 1
+	      | Black pc -> is_valid_for pc b mv -1
       | Castle Queenside White _ -> 
       | Castle Queenside Black _ ->
       | Castle Kingside White _ ->
@@ -137,17 +152,17 @@ struct
               (PositionMap.add p2 pc map) in
               new_map
         | Castle Queenside White _ ->
-            exec (exec map (Standard ((0, 0), (0, 3))))
-              (Standard ((0, 4), (0, 2)))
+            exec (exec map (Standard (create_pos 0 0, create_pos 0 3)))
+              (Standard (create_pos 0 4, create_pos 0 2))
         | Castle Queenside Black _ ->
-            exec (exec map (Standard ((7, 0), (7, 3))))
-              (Standard ((7, 4), (7, 2)))
+            exec (exec map (Standard (create_pos 7 0, create_pos 7 3)))
+              (Standard (create_pos 7 4, create_pos 7 2))
         | Castle Kingside White _ ->
-            exec (exec map (Standard ((0, 7), (0, 5))))
-              (Standard ((0, 4), (0, 6)))
+            exec (exec map (Standard (create_pos 0 7, create_pos 0 5)))
+              (Standard (create_pos 0 4, create_pos 0 6))
         | Castle Kingside Black _ ->
-            exec (exec map (Standard ((7, 7), (7, 5))))
-              (Standard ((7, 4), (7, 6)))
+            exec (exec map (Standard (create_pos 7 7, create_pos 7 5)))
+              (Standard (create_pos 7 4, create_pos 7 6))
     in
       if is_valid b mv then 
         let (map, cfg) = b in
