390bfdb4977486781a882c5d831ea3a06cdec898
David Palmer <dpalmer@college.harvard.edu>
2011-04-01 02:16:10 -0400

    Implemented standard evaluator module.

--- engine.ml
+++ engine.ml
@@ -8,11 +8,14 @@ end
 module type EVAL =
 sig
   type board
-
-  (* Calculate the board's value to the current player. *)
   type value
-  val eval : board -> value
-  val compare : value -> value -> Order.order
+  type evaluator
+  val min_value : value (* minimum possible value *)
+  val comp : value -> value -> Order.order
+  val max : value -> value -> value
+  val apply : evaluator -> board -> value
+  val init_eval : evaluator (* standard evaluator *)
+  val train : evaluator -> evaluator (* learning function *)
 end
 
 module type ENGPARAMS =
@@ -27,37 +30,72 @@ sig
   val strat : board -> move option
 end
 
+module StdEval (B : BOARD) =
+struct
+  type board = B.board
+  type value = int
+  type evaluator = board -> value
+  let min_value = -1000000
+  let comp a b =
+    let cmp = compare a b in
+    if cmp > 0 then Greater
+    else if cmp = 0 then Equal
+    else Less
+  let max = max
+  let apply eval bd = eval bd
+  let init_eval bd =
+    let pcs = B.all_pieces bd in
+    let pc_side pc =
+      match pc with
+        | Black _ -> -1
+        | White _ -> 1
+    in
+    let pc_val pc =
+      (match pc with
+         | Black Pawn | White Pawn -> 1
+         | Black Knight | White Knight -> 3
+         | Black Bishop | White Bishop -> 3
+         | Black Rook | White Rook -> 5
+         | Black Queen | White Queen -> 9
+         | Black King | White King -> 0
+      ) * pc_side pc * pc_side (B.to_play bd)
+    in
+      List.fold_left (fun r elt -> let (_, pc) = elt in r + pc_val pc) 0 pcs
+  let train eval = eval
+end
+
 (* an engine using alpha-beta search based on an evaluator *)
-module ABSEngine (B : BOARD) (L : EVAL with type board = B.board and type value = int)
+module ABSEngine (B : BOARD) (L : EVAL with type board = B.board)
   (R : ENGPARAMS) :
   (ENGINE with type board = B.board and type move = B.move) =
 struct
   type board = B.board
   type move = B.move
 
-  let move_eval bd mv =
+  let move_eval bd mv eval =
     let rec move_eval_r n bd =
-      if n = 0 then L.eval bd
+      if n = 0 then L.apply eval bd
       else
         let moves = B.all_moves bd in
           match moves with
-            | [] -> L.eval
+            | [] -> L.apply eval bd
             | _ -> 
                 let boards = List.map (B.play bd) moves in
                 let values = List.map (move_eval_r (n - 1)) boards in
-                  List.fold_left max -1000000 values
+                  List.fold_left L.max min_value values
     in
       move_eval_r R.depth (B.play bd mv)
 
-  let strat bd =
+  let strat bd eval =
     let moves = B.all_moves bd in
     let better_move mv1 mv2 =
-      if move_eval bd mv1 > move_eval bd mv2 then mv1
-      else mv2
+      match comp (move_eval bd mv1 eval) (move_eval bd mv2 eval) with
+	| Greater -> mv1
+        | Equal | Less -> mv2
     in
       match moves with
         | [] -> None
-        | hd::tl -> 
+        | hd::tl ->
             Some (List.fold_left better_move hd moves)
 end
 
