c66a956a065736aa12120d2f69ccd364a95653bb
David Palmer <dpalmer@college.harvard.edu>
2011-04-01 00:20:50 -0400

    Implemented basic negamax algorithm--no αβ pruning yet.

--- engine.ml
+++ engine.ml
@@ -10,6 +10,8 @@ end
 module type EVAL =
 sig
   type board
+
+  (* Calculate the board's value to the current player. *)
   type value
   val eval : board -> value
   val compare : value -> value -> Order.order
@@ -25,23 +27,41 @@ module type ENGINE =
 sig
   type board
   type move
-  val strat : board -> move
+  val strat : board -> move option
 end
 
 (* an engine using alpha-beta search based on an evaluator *)
-module ABSEngine (B : BOARD) (L : EVAL with type board = B.board)
+module ABSEngine (B : BOARD) (L : EVAL with type board = B.board and type value = int)
   (R : ENGPARAMS) :
   (ENGINE with type board = B.board and type move = B.move) =
 struct
   type board = B.board
   type move = B.move
-  
-  let strat bd =
-    let rec strat_r bd maxval level =
-      let moves = B.all_moves bd in
-      let boards = List.map (play B.bd) moves in
-      let 
+
+  let move_eval bd mv =
+    let rec move_eval_r n bd =
+      if n = 0 then L.eval bd
+      else
+        let moves = B.all_moves bd in
+          match moves with
+            | [] -> L.eval
+            | _ -> 
+                let boards = List.map (B.play bd) moves in
+                let values = List.map (move_eval_r (n - 1)) boards in
+                  List.fold_left max -1000000 values
     in
+      move_eval_r R.depth (B.play bd mv)
+
+  let strat bd =
+    let moves = B.all_moves bd in
+    let better_move mv1 mv2 =
+      if move_eval bd mv1 > move_eval bd mv2 then mv1
+      else mv2
+    in
+      match moves with
+        | [] -> None
+        | hd::tl -> 
+            Some (List.fold_left better_move hd moves)
       
 end
 
