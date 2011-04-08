2df51b292fa01c9bbf1a26daf779b1e071b9adb8
David Palmer <dpalmer@college.harvard.edu>
2011-04-08 03:40:02 -0400

    Made the server more portable.

--- server.ml
+++ server.ml
@@ -63,8 +63,7 @@ let strip_headers post =
 let local_path qs =
   let url_re = Str.regexp_string_case_fold server_url in
   let path = Str.global_replace url_re "/" qs in
-    Printf.printf "%s\n" server_url;
-    Printf.printf "%s\n" path;
+  let path = "index.html" in
     Filename.concat (Unix.getcwd()) path
 
 (* read in all the lines from a file and concatenate them into
