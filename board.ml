89fdfa0109136cabc61844e6192db364f068b273
Robert Hero <robertmhero@gmail.com>
2011-04-24 14:03:06 -0400

    commented out bitboard

--- board.ml
+++ board.ml
@@ -483,7 +483,7 @@ struct
       | Bishop -> is_valid_bishop
       | Rook -> is_valid_rook
       | Queen -> is_valid_queen 
-      | King -> is_valid_queen
+      | King -> is_valid_king
 
   let is_valid bd move =
     match move with
@@ -586,7 +586,7 @@ struct
     if not (check bd) then false
     else all_moves bd = []
 end
-
+(*
 module BitBoard : BOARD =
 struct
   type bitmask = int64
@@ -778,5 +778,5 @@ struct
 
   let checkmate bd =
 end
-
+*)
 module StdBoard : BOARD = MapBoard
--- eval.ml
+++
@@ -1,5 +0,0 @@
-module FancyEval (B: BOARD) : (Eval with type board = B.board) =
-struct 
-  type board = B.board
-  type value = Finite of int | Inf | NInf
-  type evaluator = board ->  
